88405aaf3e2b0cceb4c22e8b6864d2320e346b9a
alwry <144924749+alwry@users.noreply.github.com>
2024-04-12 21:52:17 -0400

    Promo + burger
    Added JS code for promo and burger menu

---
+++ js/overlay.js
@@ -0,0 +1,15 @@
+let sendButton = document.querySelector('#send-button'),
+    closeButton = document.querySelector('#close-button'),
+    popUp = document.querySelector('.popup');
+
+    function openOverlay() {
+        popUp.classList.remove('hidden')
+    }
+    
+    function closeOverlay() {
+        popUp.classList.add('hidden')
+    }
+    
+    
+    sendButton.addEventListener('click', openOverlay)
+    closeButton.addEventListener('click', closeOverlay)
